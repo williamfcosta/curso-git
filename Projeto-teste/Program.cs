d8670e2e0dda155b68b7e9c0b6633900a305a784
William Costa <williamfcosta@gmail.com>
2018-06-26 21:13:29 -0300

    Acrescentada a pergunta sobre como voce esta

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -10,6 +10,8 @@ namespace Projeto_teste {
 
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?!");
+
             Console.ReadLine();
         }
     }
